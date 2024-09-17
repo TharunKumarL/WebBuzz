ecabd9f9fd218515e1495cb261b5fe79eddb82e8
Tharun Kumar L <154898153+TharunKumarL@users.noreply.github.com>
2024-09-17 21:53:19 +0530

    commit

--- cricinfo/src/App.js
+++ cricinfo/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Main from './Components/Main';
 import './css/App.css';
 
 function App() {
